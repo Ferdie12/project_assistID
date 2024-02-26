2013d94427c3317ad81b85ad848180ff6c4f88e4
Ferdie12 <maulanaferdie1@gmail.com>
2024-02-27 00:21:47 +0700

    fix: seeder karyawan-seed edit

--- service_company_B/src/seeder/karyawan-seed.js
+++ service_company_B/src/seeder/karyawan-seed.js
@@ -1,4 +1,5 @@
 import { UserModel } from "../models/user-schema.js";
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import user from "./data/karyawan.json" assert { type: "json" };
@@ -7,10 +8,18 @@ dotenv.config();
 const insertUsers = async (userData) => {
   try {
     await mongoose.connect(process.env.DATABASE_URI);
-    const result = await UserModel.insertMany(userData, {
+
+    // Enkripsi password untuk setiap pengguna
+    const hashedUsers = userData.map((user) => {
+      const hashedPassword = bcrypt.hashSync(user.password, 10);
+      return { ...user, password: hashedPassword };
+    });
+
+    const result = await UserModel.insertMany(hashedUsers, {
       bufferCommands: false,
     });
     console.log("Data inserted successfully:", result);
+
     await mongoose.disconnect(process.env.DATABASE_URI);
     return result;
   } catch (error) {
